4e8fd6f66c0341bde6cc86e81dbc5a1003294e37
Jahangir Alisoy <jahangir.alisoy.std@bhos.edu.az>
2026-06-08 05:09:33 +0400

    fix: bug fixed

--- src/main/java/teknofest/signa/producer/client/BankAClient.java
+++ src/main/java/teknofest/signa/producer/client/BankAClient.java
@@ -1,13 +1,11 @@
 package teknofest.signa.producer.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import teknofest.signa.producer.dto.CustomerInfo;
 
-@Component
-@FeignClient(name = "bank-a", url = "${feign.client.config.bank-a.url")
+@FeignClient(name = "bank-a", url = "${feign.client.config.bank-a.url}")
 public interface BankAClient {
 
     @GetMapping("/api/v1/search")
--- src/main/java/teknofest/signa/producer/client/BankBClient.java
+++ src/main/java/teknofest/signa/producer/client/BankBClient.java
@@ -1,13 +1,11 @@
 package teknofest.signa.producer.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import teknofest.signa.producer.dto.CustomerInfo;
 
-@Component
-@FeignClient(name = "bank-b", url = "${feign.client.config.bank-b.url")
+@FeignClient(name = "bank-b", url = "${feign.client.config.bank-b.url}")
 public interface BankBClient {
 
     @GetMapping("/api/v1/search")
