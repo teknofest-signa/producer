4fff795d55d2f6e759a71d3f4f924f20d3ce52a4
Jahangir Alisoy <jahangir.alisoy.std@bhos.edu.az>
2026-06-14 13:11:48 +0400

    fix: database type mismatch issue

--- src/main/java/teknofest/signa/producer/model/entity/Admin.java
+++ src/main/java/teknofest/signa/producer/model/entity/Admin.java
@@ -1,15 +1,12 @@
 package teknofest.signa.producer.model.entity;
 
-import jakarta.persistence.Basic;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.Lob;
 import jakarta.persistence.Table;
 import java.time.Instant;
 import java.util.UUID;
@@ -20,7 +17,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.type.SqlTypes;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -65,8 +64,7 @@ public class Admin {
     @Column(name = "role", nullable = false)
     Role role = Role.ADMIN;
 
-    @Lob
-    @Basic(fetch = FetchType.LAZY)
+    @JdbcTypeCode(SqlTypes.VARBINARY)
     @Column(name = "profile_photo")
     byte[] profilePhoto;
 
