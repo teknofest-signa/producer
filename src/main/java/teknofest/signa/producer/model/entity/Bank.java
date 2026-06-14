e90761e257a411e92dfae9dcf6a9026375360d91
Jahangir Alisoy <jahangir.alisoy.std@bhos.edu.az>
2026-06-14 14:38:45 +0400

    feature: add database layers

--- src/main/java/teknofest/signa/producer/model/entity/Bank.java
+++ src/main/java/teknofest/signa/producer/model/entity/Bank.java
@@ -3,8 +3,6 @@ package teknofest.signa.producer.model.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
@@ -17,14 +15,10 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
-import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.annotations.UuidGenerator;
-import org.hibernate.type.SqlTypes;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import teknofest.signa.producer.enums.Role;
-import teknofest.signa.producer.enums.Status;
 
 @Entity
 @Getter
@@ -32,41 +26,18 @@ import teknofest.signa.producer.enums.Status;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "admins")
+@Table(name = "banks")
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @EntityListeners(AuditingEntityListener.class)
-public class Admin {
+public class Bank {
 
     @Id
     @UuidGenerator
     @GeneratedValue
     UUID id;
 
-    @Column(name = "username", nullable = false)
-    String username;
-
-    @Column(name = "email", nullable = false)
-    String email;
-
-    @Column(name = "password", nullable = false)
-    String password;
-
-    @Column(name = "token")
-    String token;
-
-    @Builder.Default
-    @Enumerated(EnumType.STRING)
-    @Column(name = "status", nullable = false)
-    Status status = Status.ACTIVE;
-
-    @Builder.Default
-    @Enumerated(EnumType.STRING)
-    @Column(name = "role", nullable = false)
-    Role role = Role.ADMIN;
-
-    @JdbcTypeCode(SqlTypes.VARBINARY)
-    @Column(name = "profile_photo")
-    byte[] profilePhoto;
+    @Column(name = "name", nullable = false)
+    String name;
 
     @CreatedDate
     @Column(name = "created_at", nullable = false, updatable = false)
