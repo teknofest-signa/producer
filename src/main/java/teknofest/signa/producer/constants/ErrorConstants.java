7c698f33d2a17fdef467df303456f71b9b8c3904
Jahangir Alisoy <jahangir.alisoy.std@bhos.edu.az>
2026-06-14 17:12:37 +0400

    feature: add transaction service

--- src/main/java/teknofest/signa/producer/constants/ErrorConstants.java
+++ src/main/java/teknofest/signa/producer/constants/ErrorConstants.java
@@ -8,10 +8,14 @@ public class ErrorConstants {
 
     public static final String ADMIN_NOT_FOUND = "Admin not found!";
     public static final String TOKEN_NOT_FOUND = "Token not found!";
+    public static final String CARD_NOT_FOUND = "Card not found!";
+    public static final String ACCOUNT_NOT_FOUND = "Account not found!";
 
     public static final String EMAIL_ALREADY_EXISTS = "Email already exists!";
     public static final String USERNAME_ALREADY_EXISTS = "Username already exists!";
 
     public static final String FAILED_TO_UPLOAD_PHOTO = "Failed to upload photo!";
+
+    public static final String INSUFFICIENT_BALANCE = "Insufficient balance!";
 }
 
