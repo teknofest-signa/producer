14dcd528eb3735145aae34b6abf4895944b92609
Jahangir Alisoy <jahangir.alisoy.std@bhos.edu.az>
2026-06-14 16:40:28 +0400

    feature: add dto and repository levels

---
+++ src/main/java/teknofest/signa/producer/model/dto/transaction/TransactionResponse.java
@@ -0,0 +1,17 @@
+package teknofest.signa.producer.model.dto.transaction;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class TransactionResponse {
+    Integer score;
+}
