3dad929fa72a3b0808a9e17ab985826595ff31f9
Jahangir Alisoy <jahangir.alisoy.std@bhos.edu.az>
2026-06-16 01:32:17 +0400

    feature: new service to get all simulated transactions

--- src/main/java/teknofest/signa/producer/service/SimulationService.java
+++ src/main/java/teknofest/signa/producer/service/SimulationService.java
@@ -3,11 +3,16 @@ package teknofest.signa.producer.service;
 import java.math.BigDecimal;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import teknofest.signa.producer.enums.SimulationType;
 import teknofest.signa.producer.enums.TransactionFraudStatus;
 import teknofest.signa.producer.model.dto.simulation.SimulateTransactionRequest;
 import teknofest.signa.producer.model.dto.simulation.SimulateTransactionResponse;
+import teknofest.signa.producer.model.dto.simulation.SimulationTransactionInfo;
 import teknofest.signa.producer.model.entity.Simulation;
 import teknofest.signa.producer.repository.SimulationRepository;
 
@@ -15,6 +20,8 @@ import teknofest.signa.producer.repository.SimulationRepository;
 @Service
 @RequiredArgsConstructor
 public class SimulationService {
+
+    private final static String SORT_FIELD = "createdAt";
 
     private final SimulationRepository simulationRepository;
 
@@ -74,5 +81,30 @@ public class SimulationService {
 
         simulation.setTransactionFraudScore(score);
         simulation.setTransactionFraudStatus(status);
+    }
+
+    public Page<SimulationTransactionInfo> getAllSimulationTransactions(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by(SORT_FIELD).descending());
+        return simulationRepository.findAll(pageable).map(this::toSimulationTransactionInfo);
+    }
+
+    private SimulationTransactionInfo toSimulationTransactionInfo(Simulation simulation) {
+        return SimulationTransactionInfo.builder()
+                .id(simulation.getId())
+                .amount(simulation.getAmount())
+                .currency(simulation.getCurrency())
+                .transactionChannel(simulation.getTransactionChannel())
+                .transactionTime(simulation.getTransactionTime())
+                .transactionType(simulation.getTransactionType())
+                .transactionFraudStatus(simulation.getTransactionFraudStatus())
+                .isCrossBorderTransaction(simulation.isCrossBorderTransaction())
+                .isNewBeneficiary(simulation.isNewBeneficiary())
+                .updatedAt(simulation.getUpdatedAt())
+                .createdAt(simulation.getCreatedAt())
+                .fromAccountId(simulation.getFromAccountId())
+                .toAccountId(simulation.getToAccountId())
+                .simulationType(simulation.getSimulationType())
+                .transactionFraudScore(simulation.getTransactionFraudScore())
+                .build();
     }
 }
