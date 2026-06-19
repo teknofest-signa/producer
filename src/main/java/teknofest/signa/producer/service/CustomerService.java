ab96cc42d1886a9d1e4ee496a5bb3435b3daaa82
Jahangir Alisoy <jahangir.alisoy.std@bhos.edu.az>
2026-06-19 23:37:56 +0400

    feature: add register customer service

--- src/main/java/teknofest/signa/producer/service/CustomerService.java
+++ src/main/java/teknofest/signa/producer/service/CustomerService.java
@@ -1,14 +1,22 @@
 package teknofest.signa.producer.service;
 
+import static teknofest.signa.producer.constants.ErrorConstants.BANK_NOT_FOUND;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import teknofest.signa.producer.enums.CustomerStatus;
+import teknofest.signa.producer.handler.exception.ResourceNotFoundException;
 import teknofest.signa.producer.model.dto.customer.CustomerInfo;
+import teknofest.signa.producer.model.dto.customer.RegisterCustomerRequest;
+import teknofest.signa.producer.model.entity.Bank;
 import teknofest.signa.producer.model.entity.Customer;
+import teknofest.signa.producer.repository.BankRepository;
 import teknofest.signa.producer.repository.CustomerRepository;
 
 @Slf4j
@@ -18,14 +26,16 @@ public class CustomerService {
 
     private final static String SORT_FIELD = "createdAt";
 
+    private final BankRepository bankRepository;
+    private final PasswordEncoder passwordEncoder;
     private final CustomerRepository customerRepository;
 
-    public Page<CustomerInfo> getAllSimulationCustomers(int page, int size) {
+    public Page<CustomerInfo> getAllCustomers(int page, int size) {
         Pageable pageable = PageRequest.of(page, size, Sort.by(SORT_FIELD).descending());
-        return customerRepository.findAll(pageable).map(this::toSimulationCustomerInfo);
+        return customerRepository.findAll(pageable).map(this::toCustomerInfo);
     }
 
-    private CustomerInfo toSimulationCustomerInfo(Customer customer) {
+    private CustomerInfo toCustomerInfo(Customer customer) {
         return CustomerInfo.builder()
                 .id(customer.getId())
                 .bankId(customer.getBankId())
@@ -34,5 +44,19 @@ public class CustomerService {
                 .customerStatus(customer.getCustomerStatus())
                 .createdAt(customer.getCreatedAt())
                 .build();
+    }
+
+    public void registerCustomer(RegisterCustomerRequest registerCustomerRequest) {
+        Bank bank = bankRepository.findById(registerCustomerRequest.getBankId())
+                .orElseThrow(() -> new ResourceNotFoundException(BANK_NOT_FOUND));
+
+        Customer customer = Customer.builder()
+                .name(registerCustomerRequest.getName())
+                .bankId(bank.getId().toString())
+                .bankName(bank.getName())
+                .hash(passwordEncoder.encode(registerCustomerRequest.getFin()))
+                .customerStatus(CustomerStatus.ACTIVE)
+                .build();
+        customerRepository.save(customer);
     }
 }
